1358479df623aea4495606dcf79976d0f630f4d0
Laura Garcia <lgarcialara@csumb.edu>
2017-10-12 20:09:43 +0000

    adding the connection

---
+++ dbConnection.php
@@ -0,0 +1,19 @@
+<?php
+
+function getDatabaseConnection() {
+    $host = 'us-cdbr-iron-east-05.cleardb.net'; //cloud 9 database
+    $dbname = 'heroku_997fb852ab3ceee';
+    $username = 'b841a76d65d2f3';
+    $password = '419e605e ';
+    
+    
+    //creates database connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    //we'll be able to see some errors with database
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $dbConn;
+    
+}
+?>
